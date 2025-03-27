21ca5ee7b054b6bf7f51b5a52cee39b514507e53
smartcode0108 <suchira22334@gmail.com>
2025-03-27 22:15:22 +0530

    testing

--- test/test_01.py
+++ test/test_01.py
@@ -4,3 +4,9 @@ def add(x,y):
 
 def subtract(x,y):
     return x - y
+def multiply(x,y):
+    return x * y
+def divide(x,y):
+    if y == 0:
+        raise ValueError('Cannot divide by zero!')
+    return x / y
